8efee4671c8c3ecf18248a3f32d06e762f8ea7b3
Woood99 <85289176+Woood99@users.noreply.github.com>
2025-06-17 13:22:31 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PWAInstallPrompt from './PWAInstallPrompt';
 function App() {
    return (
       <div style={{ marginTop: '32px', fontSize: '32px' }}>
-         Test app
+         Test app1
          <PWAInstallPrompt />
       </div>
    );
